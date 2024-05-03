9f0c4cc9fae719047ff6f1fc85d5099c266e777a
alizadasaleh <helas2003@gmail.com>
2024-05-03 14:51:50 +0400

    Prettier

--- src/main/java/com/ailab/ecommerce/order/OrderRepository.java
+++ src/main/java/com/ailab/ecommerce/order/OrderRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    public List<Order> findAllByCustomerId(Long customerId);
+    List<Order> findAllByCustomerId(Long customerId);
 
 }
