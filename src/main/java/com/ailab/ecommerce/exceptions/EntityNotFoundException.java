8c581f3813243dc59b55315780383ca872319692
alizadasaleh <helas2003@gmail.com>
2024-05-03 14:51:13 +0400

    Prettier

--- src/main/java/com/ailab/ecommerce/exceptions/EntityNotFoundException.java
+++ src/main/java/com/ailab/ecommerce/exceptions/EntityNotFoundException.java
@@ -3,11 +3,11 @@ package com.ailab.ecommerce.exceptions;
 
 public class EntityNotFoundException extends RuntimeException {
 
-    public EntityNotFoundException(Class entityName, Long id) {
-        super(entityName + " not found: " + id);
+    public EntityNotFoundException(Class<?> entityName, Long id) {
+        super(entityName.getSimpleName() + " not found: " + id);
     }
-    public EntityNotFoundException(Class entityName, String by) {
-        super(entityName + " not found: " + by);
+    public EntityNotFoundException(Class<?> entityName, String by) {
+        super(entityName.getSimpleName() + " not found: " + by);
     }
     public EntityNotFoundException(String message) {
         super(message);
